19750fca49e955c956ed48faef3bf76c4d19f095
Taher <ttaherhagui@gmail.com>
2022-01-31 13:54:59 +0100

    create AllBoos class to get rid off variable class and handle list all books function inside this class.

--- book.rb
+++ book.rb
@@ -1,13 +1,16 @@
+require './all_books'
 class Book
   attr_accessor :title, :author
   attr_reader :rentals
 
-  @@all_books = []
+  ALL_BOOKS = AllBooks.new
+  # @@all_books = []
   def initialize(title, author)
     @title = title
     @author = author
     @rentals = []
-    @@all_books << self
+    # @@all_books << self
+    ALL_BOOKS.all_books << self
   end
 
   def add_rental(rental)
