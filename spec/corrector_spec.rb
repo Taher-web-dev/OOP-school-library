0674733a2027a8d66f8c39a08533e46e6f1108a9
Taher <ttaherhagui@gmail.com>
2022-02-02 16:31:33 +0100

    add unit test to ensure that corrector class return name don't longer than 10 .

--- spec/corrector_spec.rb
+++ spec/corrector_spec.rb
@@ -4,4 +4,8 @@ describe Corrector do
     corrector = Corrector.new
     expect(corrector.correct_name('taher')).to eql('Taher')
   end
+  it 'Return only the first ten lettres using correct_name method' do
+    corrector = Corrector.new
+    expect(corrector.correct_name('aabbccddeeffggghayy').length).to be<=10
+  end
 end
